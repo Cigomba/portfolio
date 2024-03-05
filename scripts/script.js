11e9ed253c886eef7a3c5a805e84ba469cd69a14
milah3 <Cigombamiler@gmail.com>
2024-03-04 17:45:05 -0700

    com

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+
+const contactBtn = document.getElementById("contactme");
+
+contactBtn.onclick = () => {
+    // window.scrollTo({ left: 0, top: document.body.scrollHeight, behavior: "smooth" });
+    window.scrollTo(0, document.body.scrollHeight);
+}
