a3d68587b3db752483638c3aa5ca6166530f3454
Cigomba Miler <cigombamiler@yahoo.com>
2024-04-17 13:26:07 -0700

    new changes

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,31 @@ contactBtn.onclick = () => {
     // window.scrollTo({ left: 0, top: document.body.scrollHeight, behavior: "smooth" });
     window.scrollTo(0, document.body.scrollHeight);
 }
+
+const backToTopBtn = document.getElementById("back_to_top");
+const sticky = backToTopBtn.offsetTop;
+
+const 
+
+window.onscroll = () => {
+	if (window.scrollY >= sticky) {
+		backToTopBtn.style.display = "block";
+		backToTopBtn.classList.add("sticky");
+	} else {
+		backToTopBtn.classList.remove("sticky");
+	}
+};
+
+// backToTopBtn.on
+
+const anime = require("animejs");
+
+anime({
+	targets: "div",
+	translateX: 250,
+	rotate: "1turn",
+	backgroundColor: "#FFF",
+	duration: 800,
+});
+
+
